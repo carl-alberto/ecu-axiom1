cacb08191ea09a026b76a8c3c1f15983bf7b33b9
carl-alberto <cgalbert01@gmail.com>
2023-04-07 02:39:13 +0800

    updateconfig

--- web/wp-config.php
+++ web/wp-config.php
@@ -9,41 +9,48 @@
  */
 
 
-
-
-
-
-
-
 // ** Plugin Settings ** //
 
 //https://wordpress.org/plugins/redis-cache/#other_notes
-define( 'WP_REDIS_DISABLE_BANNERS', true );
-define( 'WP_REDIS_HOST', getenv('REDIS_HOST') );
-define( 'WP_REDIS_PASSWORD', getenv('REDIS_PASSWORD') );
+define('WP_REDIS_DISABLE_BANNERS', true);
+define('WP_REDIS_HOST', getenv('REDIS_HOST'));
+define('WP_REDIS_PASSWORD', getenv('REDIS_PASSWORD'));
 
-/** Disable Redis If true ( set to true if redis is not installed ) */
+/**
+ * Disable Redis If true ( set to true if redis is not installed ) 
+*/
 define('WP_REDIS_DISABLED', filter_var(getenv('WP_REDIS_DISABLED'), FILTER_VALIDATE_BOOLEAN));
 
 // Avoids conflicts with plugins including older versions of select 2 for the shortcake ui.
-define( 'SELECT2_NOCONFLICT', true );
+define('SELECT2_NOCONFLICT', true);
 
 
-/** This provides PHP information from load balancer and allows https requests */
-if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+/**
+ * This provides PHP information from load balancer and allows https requests 
+*/
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
     $_SERVER['HTTPS']='on';
+}
 
-/** Turn off/on Analytics. This is not just Google but all scripts that we have added to collect data. */
-define('DISABLE_WP_ANALYTICS', filter_var(getenv('DISABLE_WP_ANALYTICS'), FILTER_VALIDATE_BOOLEAN) );
+/**
+ * Turn off/on Analytics. This is not just Google but all scripts that we have added to collect data. 
+*/
+define('DISABLE_WP_ANALYTICS', filter_var(getenv('DISABLE_WP_ANALYTICS'), FILTER_VALIDATE_BOOLEAN));
 
-/** Turn off/on Email. */
-define('DISABLE_WP_EMAIL', filter_var(getenv('DISABLE_WP_EMAIL'), FILTER_VALIDATE_BOOLEAN) );
+/**
+ * Turn off/on Email. 
+*/
+define('DISABLE_WP_EMAIL', filter_var(getenv('DISABLE_WP_EMAIL'), FILTER_VALIDATE_BOOLEAN));
 
-/** The uploads directory location. */
+/**
+ * The uploads directory location. 
+*/
 // is this relevant??
-#define('UPLOADS', 'wp-content/pv-uploads');
+// define('UPLOADS', 'wp-content/pv-uploads');
 
-/** Disable API Calls */
+/**
+ * Disable API Calls 
+*/
 define('WP_HTTP_BLOCK_EXTERNAL', filter_var(getenv('WP_HTTP_BLOCK_EXTERNAL'), FILTER_VALIDATE_BOOLEAN));
 
 /**
@@ -57,36 +64,48 @@ define('WP_ACCESSIBLE_HOSTS', getenv('WP_ACCESSIBLE_HOSTS'));
  */
 define('WP_IFRAME_HOSTS', getenv('WP_IFRAME_HOSTS'));
 
-/** Disable all automatic updates: */
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
+/**
+ * Disable all automatic updates: 
+*/
+define('AUTOMATIC_UPDATER_DISABLED', true);
 
 
 
 
 
-/** Disable WordPress Cron */
+/**
+ * Disable WordPress Cron 
+*/
 define('DISABLE_WP_CRON', filter_var(getenv('DISABLE_WP_CRON'), FILTER_VALIDATE_BOOLEAN));
 
-/** Make sure a cron process cannot run more than once every WP_CRON_LOCK_TIMEOUT seconds. **/
-define( 'WP_CRON_LOCK_TIMEOUT', 180 );
+/**
+ * Make sure a cron process cannot run more than once every WP_CRON_LOCK_TIMEOUT seconds. 
+**/
+define('WP_CRON_LOCK_TIMEOUT', 180);
 
-/** Default Theme **/
-define( 'WP_DEFAULT_THEME', 'gangplank' );
+/**
+ * Default Theme 
+**/
+define('WP_DEFAULT_THEME', 'gangplank');
 
-/** ECU CDN URLs **/
+/**
+ * ECU CDN URLs 
+**/
 define('CDN_IMAGE_URL', getenv('CDN_IMAGE_URL'));
 define('CDN_VIDEO_URL', getenv('CDN_VIDEO_URL'));
 
-/** ECU LDAP Settings **/
-define('LDAP_DN','dc=intra,dc=ecu,dc=edu');
-define('LDAP_HOST','shipahoy.ecu.edu');
-define('LDAP_PASSWORD',getenv('LDAP_PASSWORD'));
-define('LDAP_USERNAME',getenv('LDAP_USERNAME'));
-define('LDAP_SERVER_PORT','389');
-define('LDAP_SUFFIX','@ecu.edu');
-define('LDAP_STUDENT_SUFFIX','@students.ecu.edu');
+/**
+ * ECU LDAP Settings 
+**/
+define('LDAP_DN', 'dc=intra,dc=ecu,dc=edu');
+define('LDAP_HOST', 'shipahoy.ecu.edu');
+define('LDAP_PASSWORD', getenv('LDAP_PASSWORD'));
+define('LDAP_USERNAME', getenv('LDAP_USERNAME'));
+define('LDAP_SERVER_PORT', '389');
+define('LDAP_SUFFIX', '@ecu.edu');
+define('LDAP_STUDENT_SUFFIX', '@students.ecu.edu');
 // 0 = none, 1 = ssl, 2 = tls
-define('LDAP_ENCRYPTION',0);
+define('LDAP_ENCRYPTION', 0);
 
 /* Multisite  */
 define('MULTISITE', true);
@@ -101,52 +120,54 @@ define('BLOG_ID_CURRENT_SITE', getenv('BLOG_ID_CURRENT_SITE'));
  * Store secrets in a private folder.
  */
 if (file_exists(dirname(__FILE__) . '/wp-content/uploads/private/wp-config-secrets.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-	require_once(dirname(__FILE__) . '/wp-content/uploads/private/wp-config-secrets.php');
-
+    include_once dirname(__FILE__) . '/wp-content/uploads/private/wp-config-secrets.php';
+}
 
 
 /**
  * Pantheon platform settings. Everything you need should already be set.
  */
 if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
+    include_once dirname(__FILE__) . '/wp-config-pantheon.php';
 
-/**
- * Local configuration information.
- *
- * If you are working in a local/desktop development environment and want to
- * keep your config separate, we recommend using a 'wp-config-local.php' file,
- * which you should also make sure you .gitignore.
- */
-} elseif (file_exists(dirname(__FILE__) . '/wp-config-local.php') && !isset($_ENV['PANTHEON_ENVIRONMENT'])){
-	# IMPORTANT: ensure your local config does not include wp-settings.php
-	require_once(dirname(__FILE__) . '/wp-config-local.php');
+    /**
+     * Local configuration information.
+     *
+     * If you are working in a local/desktop development environment and want to
+     * keep your config separate, we recommend using a 'wp-config-local.php' file,
+     * which you should also make sure you .gitignore.
+     */
+} elseif (file_exists(dirname(__FILE__) . '/wp-config-local.php') && !isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+    // IMPORTANT: ensure your local config does not include wp-settings.php
+    include_once dirname(__FILE__) . '/wp-config-local.php';
 
-/**
- * This block will be executed if you are NOT running on Pantheon and have NO
- * wp-config-local.php. Insert alternate config here if necessary.
- *
- * If you are only running on Pantheon, you can ignore this block.
- */
+    /**
+     * This block will be executed if you are NOT running on Pantheon and have NO
+     * wp-config-local.php. Insert alternate config here if necessary.
+     *
+     * If you are only running on Pantheon, you can ignore this block.
+     */
 } else {
-	define('DB_NAME',          'database_name');
-	define('DB_USER',          'database_username');
-	define('DB_PASSWORD',      'database_password');
-	define('DB_HOST',          'database_host');
-	define('DB_CHARSET',       'utf8');
-	define('DB_COLLATE',       '');
-	define('AUTH_KEY',         'put your unique phrase here');
-	define('SECURE_AUTH_KEY',  'put your unique phrase here');
-	define('LOGGED_IN_KEY',    'put your unique phrase here');
-	define('NONCE_KEY',        'put your unique phrase here');
-	define('AUTH_SALT',        'put your unique phrase here');
-	define('SECURE_AUTH_SALT', 'put your unique phrase here');
-	define('LOGGED_IN_SALT',   'put your unique phrase here');
-	define('NONCE_SALT',       'put your unique phrase here');
+    define('DB_NAME',          'database_name');
+    define('DB_USER',          'database_username');
+    define('DB_PASSWORD',      'database_password');
+    define('DB_HOST',          'database_host');
+    define('DB_CHARSET',       'utf8');
+    define('DB_COLLATE',       '');
+    define('AUTH_KEY',         'put your unique phrase here');
+    define('SECURE_AUTH_KEY',  'put your unique phrase here');
+    define('LOGGED_IN_KEY',    'put your unique phrase here');
+    define('NONCE_KEY',        'put your unique phrase here');
+    define('AUTH_SALT',        'put your unique phrase here');
+    define('SECURE_AUTH_SALT', 'put your unique phrase here');
+    define('LOGGED_IN_SALT',   'put your unique phrase here');
+    define('NONCE_SALT',       'put your unique phrase here');
 }
 
 
-/** Standard wp-config.php stuff from here on down. **/
+/**
+ * Standard wp-config.php stuff from here on down. 
+**/
 
 /**
  * WordPress Database Table prefix.
@@ -160,7 +181,7 @@ $table_prefix = 'wp_';
  * Site specific settings if you will be using custom upstreams that will have varying  wp-configs
  */
 if (file_exists(dirname(__FILE__) . '/wp-config-unique.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-	require_once(dirname(__FILE__) . '/wp-config-unique.php');
+    include_once dirname(__FILE__) . '/wp-config-unique.php';
 }
 
 /**
@@ -175,13 +196,17 @@ if (file_exists(dirname(__FILE__) . '/wp-config-unique.php') && isset($_ENV['PAN
  */
 
 if (!defined('WP_DEBUG')) {
-	define( 'WP_DEBUG', true );
+    define('WP_DEBUG', true);
 }
 /* That's all, stop editing! Happy Pressing. */
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+/**
+ * Absolute path to the WordPress directory. 
+*/
+if (!defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+/**
+ * Sets up WordPress vars and included files. 
+*/
+require_once ABSPATH . 'wp-settings.php';
